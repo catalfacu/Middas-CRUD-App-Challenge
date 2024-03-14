5f668cf08830060fecaa4347d1875ede35cdf40a
catalfacu <cataldof21@gmail.com>
2024-03-14 19:24:28 -0300

    Ajustar la función para buscar por ID

--- src/app/api/books/[_id]/route.js
+++ src/app/api/books/[_id]/route.js
@@ -4,7 +4,18 @@ import Book from '@/models/books';
 
 //Uso esta función para obtener los datos de un libro en especifico mediante "params", que lee la url y obtiene el dato que necesito 
 export async function GET(request, { params }) {
-    connectDB()
+    try {
+        connectDB()
         const bookById = await Book.findById(params);
-    return NextResponse.json(bookById);
+        
+//Aca estoy validando si se encontro el libro que buscaba o no
+        if(!bookById) return NextResponse.json({
+            message: "Tarea no encontrada"
+        }, {status: 404})
+    
+        return NextResponse.json(bookById);
+
+    } catch (error) {
+        return NextResponse.json(error.message, {status: 400})
+    }
 };
