2baaef7c648fe3698431a9f413e6a269093db19e
catalfacu <cataldof21@gmail.com>
2024-03-14 13:12:29 -0300

    Crear modelo de book para la base de datos

---
+++ src/models/books.js
@@ -0,0 +1,34 @@
+import { Schema, model, models } from "mongoose";
+
+//Creo el esquema book para cada libro que quiera guardar en la base de datos
+const bookSchema = new Schema({
+    book_name: {
+        type: String,
+        required: [true, 'El titulo es requerido'],
+        unique: true,
+        trim: true
+    },
+    autor: {
+        type: String,
+        required: [true, 'El autor del libro es requerido'],
+        unique: false
+    },
+    release_year: {
+        type: Number,
+        required: [true, 'El año de lanzamiento es requerido'],
+        unique: false,
+
+    },
+    genre: {
+        type: String,
+        required: [true, 'El genero es requerido']
+    },
+    isFavorite: {
+        type: Boolean,
+        default: false
+    }
+
+})
+
+//Exporto el modelo 'Book' para utilizarlo en otras partes de la app
+export default models.Book || model('Book', bookSchema);
