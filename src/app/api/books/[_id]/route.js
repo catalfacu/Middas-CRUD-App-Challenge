dbc4571331aa24a7e04d85a70f4a0a7cbbfb6bb2
catalfacu <cataldof21@gmail.com>
2024-03-14 18:31:19 -0300

    Documentar el uso de esta función

--- src/app/api/books/[_id]/route.js
+++ src/app/api/books/[_id]/route.js
@@ -2,8 +2,9 @@ import { NextResponse } from "next/server";
 import { connectDB } from "@/utils/db";
 import Book from '@/models/books';
 
+//Uso esta función para obtener los datos de un libro en especifico mediante "params", que lee la url y obtiene el dato que necesito 
 export async function GET(request, { params }) {
     connectDB()
         const bookById = await Book.findById(params);
     return NextResponse.json(bookById);
-}
+};
