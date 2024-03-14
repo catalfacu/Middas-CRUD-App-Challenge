f2f7931df2d130005e665231da9c80c0d2678082
catalfacu <cataldof21@gmail.com>
2024-03-14 13:25:35 -0300

    Crear Rutas de api para el CRUD de la información

--- src/app/api/books/route.js
+++ src/app/api/books/route.js
@@ -1,10 +1,28 @@
 import { NextResponse } from "next/server";
 import { connectDB } from "@/utils/db";
+import Book from '@/models/books';
 
 
-export function GET() {
+export async function GET() {
     connectDB();
+
+    const books = await Book.find();
+
+    return NextResponse.json(books);
+};
+
+export function POST() {
     return NextResponse.json({
-        message: "Hello World"
+        message: 'Posting element'
     })
-}
+};
+export function PUT() {
+    return NextResponse.json({
+        message: 'Posting element'
+    })
+};
+export function DELETE() {
+    return NextResponse.json({
+        message: 'Posting element'
+    })
+};
