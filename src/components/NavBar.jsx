1e055bc927d92b87e365b7ebe7cda24140945ba4
catalfacu <cataldof21@gmail.com>
2024-03-14 12:30:01 -0300

    Importar NavBar a Layout y Estilar NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 function NavBar() {
   return (
-    <div>NavBar</div>
+    <nav className='bg-slate-700 text-white flex justify-between items-center p-3'>
+        <h2>Middas Library</h2>
+        <ul className='flex justify-end gap-5'>
+            <li>Home</li>
+            <li>Favoritos</li>
+            <li>Crear+</li>
+        </ul>
+    </nav>
   )
 }
 
