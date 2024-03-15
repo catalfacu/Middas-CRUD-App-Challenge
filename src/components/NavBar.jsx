590cc11af71c4894f260671abb5920f5d133dc9a
catalfacu <cataldof21@gmail.com>
2024-03-15 13:41:29 -0300

    Agregar navegacion en navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
+import Link from 'next/link';
 
 function NavBar() {
   return (
     <nav className='bg-slate-700 text-white flex justify-between items-center p-3'>
-        <h2>Middas Library</h2>
+        <h1>Middas Library</h1>
         <ul className='flex justify-end gap-5'>
-            <li>Home</li>
-            <li>Favoritos</li>
-            <li>Crear+</li>
+            <Link href='/'> Home </Link>
+            <Link href='/books/favorites'> Favoritos </Link>
+            <Link href='/books/new'> Crear+ </Link>
         </ul>
     </nav>
   )
